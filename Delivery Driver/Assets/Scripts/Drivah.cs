27105580783eefdccff36b1b4e64b75b973579a0
Grace Fowler <f.llfr.mgrace@gmail.com>
2023-09-06 22:58:21 -0400

    Using Time.deltaTime

--- Delivery Driver/Assets/Scripts/Drivah.cs
+++ Delivery Driver/Assets/Scripts/Drivah.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Drivah : MonoBehaviour
 {
-    [SerializeField]float steerSpeed = 1f;
-    [SerializeField]float moveSpeed = 0.01f;
+    [SerializeField]float steerSpeed = 300f;
+    [SerializeField]float moveSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +15,8 @@ public class Drivah : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") *steerSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
